303cf4e8d3e4ccb64931856640d12990ef3abaff
AndrewTheWusky <121670301+AndrewTheWusky@users.noreply.github.com>
2023-01-12 03:01:57 +1000

    Add files via upload

---
+++ AdancedAI/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+author "Andrew Development"
+discription "Better AI, Make them pull over."
+games { 'gta5' }
+verison "1.0.0"
+
+
+data_file 'HANDLING_FILE' 'handling.meta'
+
+files {
+	'handling.meta'
+}
